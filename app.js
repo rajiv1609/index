1a1ae9a7c490e3432352ec2542375947380efe2e
Rajiv <rajivdwivedi110085@gmail.com>
2023-05-17 20:44:00 +0530

    react

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const heading = React.createElement("div", { id: "perant" }, [React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "Hello warld this from react"), React.createElement("h2", {}, "i am second element")]), React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "Hello warld this from react"), React.createElement("h2", {}, "i am second element")])]);
+import React  from "react";
+import ReactDOM  from "react-dom/client";
+
+
+
+
+const heading = React.createElement("h1", {id: "heading"}, "this is rajiv");
+console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
